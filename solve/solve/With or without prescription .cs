ecfdffdb2fbf9d4b521578307276837206da4f8e
lithen <luzon.t.06@gmail.com>
2023-06-12 17:46:19 +0500

    GOod job

---
+++ solve/solve/With or without prescription .cs
@@ -0,0 +1,15 @@
+﻿using Drugstore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drugstore 
+{
+  public enum dispensation
+    {
+        withPrescription,
+        withoutPrescription
+    }
+}
